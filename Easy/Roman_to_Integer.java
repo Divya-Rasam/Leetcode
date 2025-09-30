cdc5b354d8c557ca83c424dc9bdae46e6d74a9ea
Divya-Rasam <divyaarunrasam@gmail.com>
2025-10-01 00:06:47 +0530

    commit

---
+++ Easy/Roman_to_Integer.java
@@ -0,0 +1,22 @@
+public class Roman_to_Integer {
+    public static void main(String[] args) {
+        
+        String s = "III";
+
+        // current < next → subtract, else → add
+
+        for ( int i = 0; i < s.length()-2; i++) {
+                if (s.charAt(i) < s.charAt(i+1)) {
+                    int sub = s.charAt(i+1) - s.charAt(i);
+                } else {
+                    int add = s.charAt(i) + s.charAt(i+1);
+                }
+            }
+            // int calc = add + sub;
+            // return (add + sub);
+        }
+
+        // int roman = add + sub;
+
+
+    }
