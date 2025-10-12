61a067ec77d22ccb4f54ad09885fd484c114e675
Divya-Rasam <divyaarunrasam@gmail.com>
2025-10-12 14:08:08 +0530

    panagram

--- Super_Easy/Panagram_sentence.java
+++ Super_Easy/Panagram_sentence.java
@@ -3,5 +3,30 @@ package Super_Easy;
 public class Panagram_sentence {
     public static void main(String[] args) {
         
+    // Input: 
+    String sentence = "thequickbrownfoxjumpsoverthelazydog";
+    // Output: true
+
+    // Input: sentence = "leetcode"
+    // Output: false
+    
+    int [] array = new int[26];
+
+    for (char ch : sentence.toCharArray()) {
+        int index = ch - 'a';
+        array[index] = 1;
     }
-}
+
+
+
+    for (int index : array) {
+        if (index == 0) {
+            System.out.println("false");
+            return;
+        } 
+        
+    }
+
+            System.out.println("true");
+    }
+    }
