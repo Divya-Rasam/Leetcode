25527eea2455a7d840f921d79ee9f71c3f891cc3
Divya-Rasam <165023802+Divya-Rasam@users.noreply.github.com>
2026-05-06 22:07:23 +0530

    update

--- Day4/BubbleSortByMyself.java
+++ Day4/BubbleSortByMyself.java
@@ -1,15 +1,15 @@
-public class BubbleSort {
+public class BubbleSortByMyself {
     
     public static void main(String[] args) {
         
-        int [] num = {5, 3, 8, 2};
+        int [] num = {5, 7, 4, 6, 8, 33, 10, 57, 41, 35, 80};
 
         for (int i = 0; i < num.length - 1; i++) {
-            for (int j = 0 ; j < num.length - 1 - i; j++) {
-                if (num[j] > num[j + 1]) {
+            for (int j = 0; j < num.length - 1 - i; j++) {
+                if (num[j] > num[j+1]) {
                     int temp = num[j];
-                    num[j] = num[j + 1];
-                    num[j + 1] = temp;
+                    num[j] = num[j+1];
+                    num[j+1] = temp;
                 }
             }
         }
